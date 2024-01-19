e8a75353da951cc0ca8f025b3a921246ebc1a93f
ahalajian <andrewhalajian19@gmail.com>
2024-01-19 10:19:19 -0500

    Implement Log-in Functionality

--- mysite/nba_play/urls.py
+++ mysite/nba_play/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("guess/", views.guess, name="guess"),
     path("get_random_players/", views.get_random_players, name="get_random_players"),
-    path("guess/get_random_players/", views.get_random_players, name="guess_get_random_players")
-
-
+    path('home', views.index, name='index')
 ]
